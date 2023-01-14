59bc34936b6b727eea33ab807dd8ec0a41f6a46a
Artem Stepanyuk <aivanov757@gmail.com>
2023-01-14 23:18:46 +0300

    fix

--- src/js/position.js
+++ src/js/position.js
@@ -1,7 +1,7 @@
 export default function getPosition(oldIndex) {
-    let newIndex;
-    do {
-        newIndex = Math.floor(Math.random() * (15 + 1));
-      } while (newIndex === oldIndex);
-      return newIndex;
+  let newIndex;
+  do {
+    newIndex = Math.floor(Math.random() * (15 + 1));
+  } while (newIndex === oldIndex);
+  return newIndex;
 }
